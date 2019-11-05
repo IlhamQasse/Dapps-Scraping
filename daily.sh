ad01608a5ef10893a090e03d687a8b9b1ed54e05
spio <spio@asc-stealthfission2>
2019-11-05 18:06:18 +0000

    - mark potential chrome killing commands in case stray instances are around

--- daily.sh
+++ daily.sh
@@ -4,3 +4,7 @@ cd /home/ubuntu/dapps-scraping
 #xvfb-run -a -s "-screen 0 1200x900x16" python3 DappRadar.py 1 >> daily.log
 #python3 -u DappRadar.py 1 | tee daily.log
 xvfb-run -a -s "-screen 0 1200x900x16" python3 -u DappRadar.py 1 2>&1 | tee -a daily.log
+
+# for safety
+#kill `pidof chromedriver` 2>/dev/null
+#kill -9 `pidof google-chrome chrome` 2>/dev/null
